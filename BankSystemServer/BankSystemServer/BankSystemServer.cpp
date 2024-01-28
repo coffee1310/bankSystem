b56e6bb225d22e989e5610c7374c3019b3c3e48e
Coffee <Coffee13102006@yandex.ru>
2024-01-28 18:40:42 +0300

    This apllication will make connection between server and client. Client can create card, withdraw money, put money and transfer money.

---
+++ BankSystemServer/BankSystemServer/BankSystemServer.cpp
@@ -0,0 +1,39 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <stdio.h>
+#include <vector>
+
+#pragma comment(lib,"Ws2_32.lib")
+
+
+int main()
+{
+    WSADATA wsaData;
+    WORD DLLVersion = MAKEWORD(2, 1);
+    if (WSAStartup(DLLVersion, &wsaData) != 0) {
+        std::cout << "ERROR" << std::endl;
+        exit(1);
+    }
+
+    SOCKADDR_IN addr;
+    int size_addr = sizeof(addr);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(1111);
+    addr.sin_family = AF_INET;
+
+    SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);
+    bind(sListen, (SOCKADDR*)&addr, sizeof(addr));
+    listen(sListen, SOMAXCONN);
+
+    SOCKET newConnection;
+    newConnection = accept(sListen, (SOCKADDR*)&addr, &size_addr);
+
+    if (newConnection == 0) {
+        std::cout << "ERROR. Connection lost" << std::endl;
+    }
+    
+    return 0;
+}
