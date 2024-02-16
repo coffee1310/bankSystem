77cd6b56a8b158845cc6a8b9de0e873029f28ae0
Coffee <Coffee13102006@yandex.ru>
2024-02-16 23:14:29 +0300

    Now i added a new functions. I clear my code. I add a password hashing.

--- BankSystemServer/BankSystemServer/BankSystemServer.cpp
+++ BankSystemServer/BankSystemServer/BankSystemServer.cpp
@@ -7,6 +7,9 @@
 #include <vector>
 #include <sstream>
 #include <sqlite3.h>
+#include <openssl/evp.h>
+#include <openssl/sha.h>
+#include <iomanip>
 #pragma comment(lib,"Ws2_32.lib")
 
 SOCKET Connections[100];
@@ -15,32 +18,22 @@ int Counter = 0;
 class UserDataBase {
 public:
 	UserDataBase(SOCKET Connection) {
+		std::string password_;
 		this->createDB();
 		this->createTables();
-		std::string msg;
-		int msg_size;
-		char* res;
-		int res_size;
+		this->Connection = Connection;
 
-		msg = "Введите номер телефона: ";
-		msg_size = msg.size();
+		this->send_msg("Введите номер телефона: ");
+		this->phone_number = get_msg();
 
-		send(Connection, (char*)&msg_size, sizeof(int), NULL);
-		send(Connection, msg.c_str(), msg_size, NULL);
-		
-		recv(Connection, (char*)&res_size, sizeof(int), NULL);
-		res = new char[res_size + 1];
-		msg[msg_size] = '\0';
-		recv(Connection, res, res_size, NULL);
-		
-		this->phone_number = res;
-		msg = "Введите пароль: ";
-		msg_size = msg.size();
+		this->send_msg("Введите ФИО: ");
+		this->FIO = get_msg();
 
-		send(Connection, (char*)&msg_size, sizeof(int), NULL);
-		send(Connection, msg.c_str(), msg_size, NULL);
+		this->send_msg("Введите пароль: ");
+		password_ = get_msg();
+		this->password = md5(password_);
+		this->addUser();
 	}
-
 private:
 	const char* dir = R"(Bank.db)";
 	std::string phone_number;
@@ -50,20 +43,23 @@ private:
 	
 	int send_msg(std::string msg) {
 		int msg_size;
+		char* res;
+		int res_size;
+		msg_size = msg.size();
 		send(this->Connection, (char*)&msg_size, sizeof(int), NULL);
 		send(this->Connection, msg.c_str(), msg_size, NULL);
 		return 0;
 	}
 
-	char* get_msg() {
-		char* res;
+	std::string get_msg() {
 		int res_size;
-		std::string msg;
-		int msg_size;
-		recv(Connection, (char*)&res_size, sizeof(int), NULL);
-		res = new char[res_size + 1];
-		msg[msg_size] = '\0';
-		recv(Connection, res, res_size, NULL);
+		recv(this->Connection, (char*)&res_size, sizeof(int), NULL);
+		char* res = new char[res_size + 1];
+		res[res_size] = '\0';
+		recv(this->Connection, res, res_size, NULL);
+		std::string result(res);
+		delete[] res;
+		return result;
 	}
 	
 	int createDB() {
@@ -83,7 +79,7 @@ private:
 		sql = "CREATE TABLE IF NOT EXISTS Users ( "
 			"ID INTEGER PRIMARY KEY AUTOINCREMENT, "
 			"PHONE_NUMBER VARCHAR(32) NOT NULL, "
-			"PASSWORD VARCHAR(8) NOT NULL, "
+			"PASSWORD VARCHAR(64) NOT NULL, "
 			"FIO TEXT NOT NULL"
 			");";
 
@@ -159,7 +155,7 @@ private:
 	int addUser() {
 		sqlite3* DB;
 		std::string sql = "INSERT INTO Users (PHONE_NUMBER, PASSWORD, FIO) "
-			"VALUES (" + this->phone_number + ", " + this->password + ", " + this->FIO + ")";
+			"VALUES ('" + this->phone_number + "', '" + this->password + "', '" + this->FIO + "')";
 		char* err_msg;
 
 		sqlite3_open(this->dir, &DB);
@@ -171,14 +167,51 @@ private:
 				std::cerr << "Database error" << std::endl;
 				sqlite3_free(err_msg);
 			}
+			sqlite3_close(DB);
 
-			delete err_msg;
 		}
 		catch (const std::exception& e)
 		{
 			std::cerr << e.what();
+			std::cout << "error" << std::endl;
 		}
 		return 0;
+	}
+
+	std::string md5(const std::string& input) {
+		EVP_MD_CTX* ctx = EVP_MD_CTX_new();
+		if (!ctx) {
+			std::cerr << "Error creating context" << std::endl;
+			return "";
+		}
+
+		if (1 != EVP_DigestInit_ex(ctx, EVP_md5(), nullptr)) {
+			std::cerr << "Error initializing MD5" << std::endl;
+			EVP_MD_CTX_free(ctx);
+			return "";
+		}
+
+		if (1 != EVP_DigestUpdate(ctx, input.c_str(), input.size())) {
+			std::cerr << "Error updating MD5" << std::endl;
+			EVP_MD_CTX_free(ctx);
+			return "";
+		}
+
+		unsigned char digest[EVP_MAX_MD_SIZE];
+		unsigned int digestLength;
+		if (1 != EVP_DigestFinal_ex(ctx, digest, &digestLength)) {
+			std::cerr << "Error finalizing MD5" << std::endl;
+			EVP_MD_CTX_free(ctx);
+			return "";
+		}
+
+		std::stringstream ss;
+		for (unsigned int i = 0; i < digestLength; ++i) {
+			ss << std::hex << std::setw(2) << std::setfill('0') << (int)digest[i];
+		}
+
+		EVP_MD_CTX_free(ctx);
+		return ss.str();
 	}
 
 	bool checkPhoneNumber()
@@ -210,18 +243,15 @@ void ClientHandler(int index) {
         recv(Connections[index], msg, msg_size, NULL);
 		std::cout << msg << std::endl;
         for (int i = 0; i < Counter; i++) {
-            if (i == index) {
+			if (i == index) {
 				std::string result;
 				int result_size;
 				result = do_command(msg, Connections[i]);
 				result_size = result.size();
 				send(Connections[i], (char*)&result_size, sizeof(int), NULL);
 				send(Connections[i], result.c_str(), result_size, NULL);
-                continue;
-            }
-
-            /*send(Connections[i], (char*)&msg_size, sizeof(int), NULL);
-            send(Connections[i], msg, msg_size, NULL);*/
+				continue;
+			}
         }
         delete[] msg;
     }
